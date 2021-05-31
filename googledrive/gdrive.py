9e5889d8f6a5fae660d949c4b1ec559c00776688
mdauthentic <muideen.lawal20@gmail.com>
2021-06-01 00:07:48 +0200

    add google drive file manager

---
+++ googledrive/gdrive.py
@@ -0,0 +1,32 @@
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
+from pathlib import Path
+
+
+class GoogleDriveAPI:
+    def __init__(self) -> None:
+        self.drive = self.__auth()
+
+    def __auth(self) -> GoogleDrive:
+        gauth = GoogleAuth()
+        gauth.LocalWebserverAuth()
+        return GoogleDrive(gauth)
+
+    def create_folder(self, parent_folder_id: str, subfolder_name: str):
+        newFolder = self.drive.CreateFile(
+            {
+                "title": subfolder_name,
+                "parents": [{"kind": "drive#fileLink", "id": parent_folder_id}],
+                "mimeType": "application/vnd.google-apps.folder",
+            }
+        )
+        newFolder.Upload()
+        return newFolder
+
+    def upload_file(self, dir: str):
+        basepath = Path(dir)
+        for x in basepath.iterdir():
+            if x.is_file():
+                f = self.drive.CreateFile({"title": x})
+                f.SetContentFile(x)
+                f.Upload()
